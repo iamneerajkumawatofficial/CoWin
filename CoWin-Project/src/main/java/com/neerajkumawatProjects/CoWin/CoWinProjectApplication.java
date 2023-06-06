dc5b1752f8cd1f71c6994b981353ad912a5d1460
Neeraj Kumawat <130247527+iamneerajkumawatofficial@users.noreply.github.com>
2023-06-06 17:19:16 +0530

    CoWin
    Co-WIN Public APIs allow any third-party application to access certain un-restricted information, that can be shared with its users. This is limited only to read access in Co-WIN. The extent of access will be limited and in case of any misuse impacting the performance of Co-WIN solution will result in blocking any such application .

---
+++ CoWin-Project/src/main/java/com/neerajkumawatProjects/CoWin/CoWinProjectApplication.java
@@ -0,0 +1,13 @@
+package com.neerajkumawatProjects.CoWin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoWinProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoWinProjectApplication.class, args);
+	}
+
+}
